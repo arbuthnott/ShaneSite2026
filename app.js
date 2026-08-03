257d0d85346f34e5dc5edb3cb67eec8b4c074907
WorkLaptop\Chris <chris.arbuthnott@gmail.com>
2026-08-03 12:09:14 -0300

    finished contact form setup

--- app.js
+++ app.js
@@ -2,9 +2,62 @@
 
 const express = require('express');
 const app = express();
-const port = 80;
+const config = require('./config.js');
+const port = config.port;
+
+// set up the emailer options
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: config.gmailUser,
+        pass: config.gmailAppPassword
+        //clientId: config.gmailClientId,
+        //clientSecret: config.gmailClientSecret,
+        //refreshToken: config.gmailRefreshToken
+    }
+});
 
 app.use(express.static('public', {extensions: ['html', 'htm']}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.post('/api/contact', function(req, res) {
+    var msg = req.body.message;
+    var subj = req.body.subject;
+    var email = req.body.email;
+    
+    if (!msg) {
+        console.log('EMPTY MESSAGE detected in contact form post');
+        return res.send('empty');
+    }
+    
+    // do our throttling-by-obscurity
+    var seenHintSources = req.body.seenHintSources || '';
+    var requiredSources = ['message input', 'message blur', 'wait'];
+    for (var idx = 0; idx < requiredSources.length; idx++) {
+        if (seenHintSources.indexOf(requiredSources[idx]) == -1) {
+            console.log('MISSING HINT SOURCE in contact post. Posted hints: ' + seenHintSources);
+            return res.send('error');
+        }
+    }
+    
+    // still here? We can send. Recall, the passed in email is only being sent as content, not a recipient
+    var mailData = {
+        from: config.gmailUser,
+        to: config.emailRecipient,
+        subject: 'WEBSITE CONTACT FORM: ' + (subj || '(no subject)'),
+        text: 'EMAIL: ' + email + '\nMESSAGE:\n' + msg
+    };
+    transporter.sendMail(mailData, function(err, info) {
+        if (err) {
+            console.log('ERROR DURING MAIL SEND:');
+            console.log(err);
+            return res.send('error');
+        }
+        return res.send('sent');
+    });
+});
 
 app.listen(port, function() {
     console.log('Shane Author Site 2026 listening on port ' + port);
--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -8,14 +8,13 @@ Library.addHumanHint = function(source) {
 setTimeout(function() { Library.addHumanHint('wait'); }, 700);
 
 Library.validateContact = function(formData) {
-    console.log('validateContact!!');
     var feedBackElem = $('#contact-form-feedback');
     var foundError = !formData.message || !formData.email || Library.humanHints < 3;
     if (!foundError) {
-        foundError = /^[^@]+@[^@]+\.[a-zA-Z]+$/i.test(formData.email);
+        foundError = !/^[^@]+@[^@]+\.[a-zA-Z]+$/i.test(formData.email);
     }
     if (foundError) {
-        var message = '<h6>Message not sent</h6> ';
+        var message = '<h6>Message not sent...</h6> ';
         message += 'Something doesn\'t seem right. Please make sure you add message content and a proper email address before sending.';
         feedBackElem.html(message).css('opacity', 1);
         return false;
@@ -25,7 +24,7 @@ Library.validateContact = function(formData) {
 };
 
 Library.sendContact = function() {
-    console.log('sendContact!!');
+    if (Library.disableSend) { return; }
     var form = $('form#contact-form');
     var formData = {
         message: form.find('#message').val().trim(),
@@ -33,15 +32,60 @@ Library.sendContact = function() {
         email: form.find('#email').val().trim()
     };
     if (!Library.validateContact(formData)) { return; }
-    console.log('input is valid. NOw post...');
-    
-    form.append('<input type="hidden" name="hintSources" value="' + Library.seenHintSources.join(',') + '"/>');
+    formData.seenHintSources = Library.seenHintSources.join(',');
+    Library.disableSend = true;
+    $.post('/api/contact', formData, function(data) {
+        var feedBackElem = $('#contact-form-feedback');
+        var form = $('form#contact-form');
+        var button = form.find('button#send-contact');
+        
+        if (data == 'sent') {
+            var message = '<h6>Message sent</h6> ';
+            message += 'Thank you for your message! I will read it soon and respond if appropriate. ';
+            message += '<br /><a id="send-another" onclick="Library.resetContactForm()" class="darkYellowLink">send another message</a>'
+            button.attr('disabled', 'disabled');
+            form.find('input, textarea').attr('disabled', 'disabled');
+            feedBackElem.html(message).css('opacity', 1);
+        } else if (data == 'empty') {
+            var message = '<h6>Message not sent...</h6> ';
+            message += 'Something doesn\'t seem right. Please make sure you add message content and a proper email address before sending.';
+            feedBackElem.html(message).css('opacity', 1);
+            Library.disableSend = false; // allow retry
+        } else if (data == 'error') {
+            var message = '<h6>Error sending message</h6> ';
+            message += 'Sorry, there was an unknown error. Please refresh the page and retry.';
+            button.attr('disabled', 'disabled');
+            form.find('input, textarea').attr('disabled', 'disabled');
+            feedBackElem.html(message).css('opacity', 1);
+        } else { // ??
+            console.log('unexpected response from contact form api:');
+            console.log(data);
+            // let's be optimistic
+            var message = '<h6>Message sent</h6> ';
+            message += 'Thank you for your message! I will read it soon and respond if appropriate. ';
+            message += '<br /><a id="send-another"  onclick="Library.resetContactForm()" class="darkYellowLink">send another message</a>'
+            button.attr('disabled', 'disabled');
+            form.find('input, textarea').attr('disabled', 'disabled');
+            feedBackElem.html(message).css('opacity', 1);
+        }
+    });
+};
+
+Library.resetContactForm = function() {
+    var form = $('form#contact-form');
+    form.find('input, textarea, button').removeAttr('disabled');
+    Library.disableSend = false;
+    form.find('#subject, #message').val('');
+    var feedBackElem = $('#contact-form-feedback');
+    feedBackElem.css('opacity', 0);
+    setTimeout(function() { feedBackElem.html(''); }, 1000);
 };
 
 $(function() {
     var form = $('form#contact-form');
     
     form.find('#message').on('input', function() { Library.addHumanHint('message input'); });
-    form.find('#message').on('blur',function() { Library.addHumanHint('message blur'); });
+    form.find('#message').on('blur', function() { Library.addHumanHint('message blur'); });
     form.find('button#send-contact').click(function() { Library.sendContact(); });
+    //form.find('#contact-form-feedback').on('click', '#send-another', function() { Library.resetContactForm(); });
 });
